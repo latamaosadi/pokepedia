26a81bc99ac4f659efe76518d84fee3c327fcae6
Robert Latamaosadi <robert.latamaosadi@gmail.com>
2023-02-14 00:34:32 +0800

    update pwa assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,81 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import svgLoader from 'vite-svg-loader'
+import type { ManifestOptions, VitePWAOptions } from 'vite-plugin-pwa'
 import { VitePWA } from 'vite-plugin-pwa'
 import * as path from 'path'
 
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  base: '/',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Pokepedia - Classic Pokedex App',
+    short_name: 'Pokepedia',
+    description: 'A complete classic-themed pokedex app that works offline',
+    start_url: '/',
+    theme_color: '#DE3C47',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't remove slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
+  registerType: 'prompt',
+  workbox: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/raw\.githubusercontent\.com\/.*/i,
+        handler: 'CacheFirst',
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+}
+const replaceOptions = { __DATE__: new Date().toISOString() }
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src'
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+  pwaOptions.strategies = 'injectManifest'
+  ;(pwaOptions.manifest as Partial<ManifestOptions>).name =
+    'PWA Inject Manifest'
+  ;(pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject'
+}
+
+if (claims) pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+  // @ts-expect-error overrides
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying) pwaOptions.selfDestroying = selfDestroying
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin(),
-    svgLoader(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/raw\.githubusercontent\.com\/.*/i,
-            handler: 'CacheFirst',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [vue(), eslintPlugin(), svgLoader(), VitePWA(pwaOptions)],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
